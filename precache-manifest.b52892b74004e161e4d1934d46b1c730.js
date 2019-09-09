123bfdbb345e8b5c39d068fa7105c90414b23852
Kelsi H <kelsimhoyle@gmail.com>
2019-09-08 21:49:27 -0600

    Updates

---
+++ precache-manifest.b52892b74004e161e4d1934d46b1c730.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b68e927cf2d4fcba7dd112ddfa88f0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f4359fdc54e8bdf05a35",
+    "url": "/clicky-game/static/css/main.9261a458.chunk.css"
+  },
+  {
+    "revision": "a705ef7811443830daf7",
+    "url": "/clicky-game/static/js/2.a95e7809.chunk.js"
+  },
+  {
+    "revision": "f4359fdc54e8bdf05a35",
+    "url": "/clicky-game/static/js/main.8db9dee1.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  },
+  {
+    "revision": "5c59ec4f4dabb874bee2b54decec6350",
+    "url": "/clicky-game/static/media/background.5c59ec4f.jpg"
+  },
+  {
+    "revision": "5756eb1605c0c02af87e99daeca4bb83",
+    "url": "/clicky-game/static/media/manatee1.5756eb16.jpg"
+  },
+  {
+    "revision": "e149b09359092f71b4319eae955eb5dc",
+    "url": "/clicky-game/static/media/manatee10.e149b093.jpg"
+  },
+  {
+    "revision": "23fb18af10892f130ac5e0de58427fcb",
+    "url": "/clicky-game/static/media/manatee11.23fb18af.jpg"
+  },
+  {
+    "revision": "9b9ebbce2e64bd75ecaee44b9d3132a0",
+    "url": "/clicky-game/static/media/manatee12.9b9ebbce.jpg"
+  },
+  {
+    "revision": "baeaf58c60fd2f28f91bd6f34e5b5272",
+    "url": "/clicky-game/static/media/manatee2.baeaf58c.jpg"
+  },
+  {
+    "revision": "3b494f69ba1ba4ff4539457959f626da",
+    "url": "/clicky-game/static/media/manatee3.3b494f69.jpg"
+  },
+  {
+    "revision": "e2b1d8ea29aa7cb464ccd3e4d9251de6",
+    "url": "/clicky-game/static/media/manatee4.e2b1d8ea.jpg"
+  },
+  {
+    "revision": "b54016b7c87c17ad366f06d93662e5c9",
+    "url": "/clicky-game/static/media/manatee5.b54016b7.jpg"
+  },
+  {
+    "revision": "9e0cea9816edbaca61ff6c407d8932cc",
+    "url": "/clicky-game/static/media/manatee6.9e0cea98.jpg"
+  },
+  {
+    "revision": "26a1589be63b01fe52c8e67fcc97dae1",
+    "url": "/clicky-game/static/media/manatee7.26a1589b.jpg"
+  },
+  {
+    "revision": "dfb7d805732327e75c5140567c8a4629",
+    "url": "/clicky-game/static/media/manatee8.dfb7d805.jpg"
+  },
+  {
+    "revision": "3baa8334cb79961e7fb2311893a6efdb",
+    "url": "/clicky-game/static/media/manatee9.3baa8334.jpg"
+  }
+]);
